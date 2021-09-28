fe9b6c6c9e8e18793f2905fc17b2b6d96e6ca5b6
Etna2705 <91506639+Etna2705@users.noreply.github.com>
2021-09-27 19:47:55 -0500

    Primer script
    Construí un vector que se llama cajita, lo visualice y calcule la media aritmética

---
+++ Primer_script.R
@@ -0,0 +1,13 @@
+
+#primer script.
+#(todo lo que escriba despues del # no lo va a leer R)
+
+#mi primer vector 
+
+cajita<-c(3,15,16,20,10)
+
+#visualizar cajita
+cajita 
+
+# como calcular la media.
+mean(cajita)
